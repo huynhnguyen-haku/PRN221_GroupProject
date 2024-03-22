aa64517551fc48330526f8fdc0e5ed7dcf51e469
huynhnguyen-haku <huynhnguyen1532@gmail.com>
2024-03-22 11:29:14 +0700

    Fix minor bug in cart

--- StyleShopping/StyleShopping/Pages/AddCart.cshtml.cs
+++ StyleShopping/StyleShopping/Pages/AddCart.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Service.Implementation;
 using Service.Interface;
+using StyleShopping.HandleRequest;
 
 namespace StyleShopping.Pages
 {
@@ -24,66 +25,14 @@ namespace StyleShopping.Pages
                 return RedirectToPage("/AccessDenied");
             }
             int a_id = (int)HttpContext.Session.GetInt32("user_id");
-            AddCart(id, 1, a_id);
+            new AddCartRequest().AddCart(id, 1, a_id);
             return RedirectToPage("./Index");
             
 
 
         }
-        public IActionResult OnPostAsync(int id, int? quantity)
-        {
-            if (HttpContext.Session.GetInt32("user_id") == null)
-            {
-                return RedirectToPage("/Login");
-            }
-            if (HttpContext.Session.GetInt32("role") != 0)
-            {
-                return RedirectToPage("/AccessDenied");
-            }
-            int a_id = (int)HttpContext.Session.GetInt32("user_id");
-            AddCart(id, quantity, a_id);
-            return RedirectToPage("./Detail?id="+ id);
+       
 
-
-
-        }
-
-        public void AddCart(int productID, int? quantity,int id)
-        {
-            var quotation = quotationService.GetQuotationUser(id);
-            if (quotation == null)
-            {
-                Quotation newQuotation = new Quotation();
-                newQuotation.QuotationStatus = "Pending";
-                newQuotation.AccountId = id;
-                newQuotation.CreateDate = DateTime.Now;
-                newQuotation = quotationService.AddQuotation(newQuotation);
-                QuotationDetail detail = new QuotationDetail();
-                detail.QuotationId = newQuotation.QuotationId;
-                detail.InteriorId = productID;
-                detail.Quantity = quantity;
-                quotationService.AddQuotationDetail(detail);
-
-
-            }
-            else
-            {
-                QuotationDetail detail = quotationService.GetQuotationDetail(quotation.QuotationId, productID);
-
-                if (detail == null)
-                {
-                    QuotationDetail newDetail = new QuotationDetail();
-                    newDetail.QuotationId = quotation.QuotationId;
-                    newDetail.InteriorId = productID;
-                    newDetail.Quantity = quantity;
-                    quotationService.AddQuotationDetail(newDetail);
-                }
-                else
-                {
-                    detail.Quantity = detail.Quantity + quantity;
-                    quotationService.UpdateQuotationDetail(detail);
-                }
-            }
-        }
+        
     }
 }
--- StyleShopping/StyleShopping/Pages/Detail.cshtml.cs
+++ StyleShopping/StyleShopping/Pages/Detail.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Service.Implementation;
 using Service.Interface;
+using StyleShopping.HandleRequest;
+using System.Reflection.Metadata;
 
 namespace StyleShopping.Pages
 {
@@ -20,6 +22,22 @@ namespace StyleShopping.Pages
             listC = interService.ListCategory();
             interior = interService.Get(id);
             return Page();
+        }
+        public IActionResult OnPostAsync()
+        {
+            if (HttpContext.Session.GetInt32("user_id") == null)
+            {
+                return RedirectToPage("/Login");
+            }
+            if (HttpContext.Session.GetInt32("role") != 0)
+            {
+                return RedirectToPage("/AccessDenied");
+            }
+            int number = int.Parse(Request.Form["quantity"]);
+            int productID = int.Parse(Request.Form["productID"]);
+            int a_id = (int)HttpContext.Session.GetInt32("user_id");
+            new AddCartRequest().AddCart(productID, number, a_id);
+            return RedirectToPage("/Detail", new { id = productID });
         }
     }
 }
