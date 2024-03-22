2ee751632de06cbab19898d9c9ac9be48404dc70
loint5201 <113002077+loint5201@users.noreply.github.com>
2024-03-22 10:27:06 +0700

    Show list vs detail
    Show list vs detail

--- StyleShopping/BussinessObject/styleContext.cs
+++ StyleShopping/BussinessObject/styleContext.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.Extensions.Configuration;
 
 namespace BussinessObject
 {
@@ -33,15 +32,8 @@ namespace BussinessObject
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer(GetConnectionString());
+                optionsBuilder.UseSqlServer("Server=localhost;Database=style;Trusted_Connection=True;uid=sa;pwd=123456;Trust Server Certificate=True");
             }
-        }
-        public string GetConnectionString()
-        {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-            IConfiguration configuration = builder.Build();
-            return configuration.GetConnectionString("SqlConnection");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ StyleShopping/Service/Implementation/ProjectService.cs
@@ -0,0 +1,30 @@
+﻿using BussinessObject;
+using Repository.Implementation;
+using Repository.Interface;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Implementation
+{
+    public class ProjectService : IProjectService
+    {
+        private IProjectRepository _repository;
+        public ProjectService()
+        {
+            _repository = new ProjectRepository();
+        }
+        public Project Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        public IEnumerable<Project> List()
+        {
+           return _repository.List();
+        }
+    }
+}
