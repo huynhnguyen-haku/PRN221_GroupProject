005249baf34933e764e1e964b5b32c6aed6b9dbb
loint5201 <113002077+loint5201@users.noreply.github.com>
2024-03-22 10:49:25 +0700

    show list blog
    show list blog

---
+++ StyleShopping/DAO/BlogDAO.cs
@@ -0,0 +1,67 @@
+﻿using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class BlogDAO
+    {
+        private static BlogDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private BlogDAO() { }
+        public static BlogDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new BlogDAO();
+                    }
+                    return instance;
+                }
+
+            }
+        }
+        public List<Blog> List()
+        {
+            List<Blog> blogs;
+            try
+            {
+
+                using (var MySale = new styleContext())
+                {
+                    blogs = MySale.Blogs.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return blogs;
+
+        }
+
+
+        public Blog Get(int id)
+        {
+            Blog blog = null;
+            try
+            {
+                using (var MySale = new styleContext())
+                {
+                    blog = MySale.Blogs.SingleOrDefault(x => x.Id == id);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return blog;
+        }
+    }
+}
---
+++ StyleShopping/Repository/Implementation/BlogRepository.cs
@@ -0,0 +1,18 @@
+﻿using BussinessObject;
+using DAO;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation
+{
+    public class BlogRepository : IBlogRepository
+    {
+        public Blog Get(int id) => BlogDAO.Instance.Get(id);
+
+        public List<Blog> List() => BlogDAO.Instance.List();
+    }
+}
